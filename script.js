70ff6fc17573bc914387c678fe3a6ded8f3c47c9
Adam-Yun <naelyunn@gmail.com>
2024-06-25 13:12:13 +0800

    Fixing wide lens issue

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ async function startCamera() {
                 // width: { min: 640, ideal: 1280, max: 1920 },
                 // height: { min: 480, ideal: 720, max: 1080 },
 
-                facingMode: { ideal: 'environment' },
+                facingMode: { ideal: 'continuous' },
                 focusDistance: { min: 0.05, ideal: 0.12, max: 0.3 }
             }
         };
